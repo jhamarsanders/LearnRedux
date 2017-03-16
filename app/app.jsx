efa15001d25ad96b1391bb9d689b8e217e4f7335
Jay Sanders <j.sanders@realitychecksystems.com>
2017-03-16 09:39:42 -0700

    first commit

---
+++ app/app.jsx
@@ -0,0 +1,15 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+
+require('style-loader!css-loader!foundation-sites/dist/foundation.min.css');
+$(document).foundation();
+
+require('style-loader!css-loader!sass-loader!applicationStyles');
+
+ReactDOM.render(
+    <p>Redux example</p>,
+    document.getElementById('app')
+);
+
+require('./redux-example');
